6df386d2389da1e52ed3f4fbd8dec5dbab554480
v1adau <oscarnvh@gmail.com>
2019-11-24 22:53:54 +0100

    Remove unused function isMobile

--- script.js
+++ script.js
@@ -179,7 +179,3 @@ const isSameDay = (date1, date2) => {
     date1.getFullYear() === date2.getFullYear()
   );
 };
-
-const isMobile = () => {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-}
