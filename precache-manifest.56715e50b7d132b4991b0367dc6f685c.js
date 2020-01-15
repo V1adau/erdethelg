280aa9dee37a60d992ea334758dbb6f68b784209
v1adau <oscarnvh@gmail.com>
2020-01-15 16:01:31 +0100

    Updates

---
+++ precache-manifest.56715e50b7d132b4991b0367dc6f685c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0010e321616f9ef19b6570384dbc0f63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00a8c86c35eb5a40f947",
+    "url": "/static/css/main.8177f433.chunk.css"
+  },
+  {
+    "revision": "5cfd35c2ceb86c868e43",
+    "url": "/static/js/2.1ebce893.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1ebce893.chunk.js.LICENSE"
+  },
+  {
+    "revision": "00a8c86c35eb5a40f947",
+    "url": "/static/js/main.0a6888c4.chunk.js"
+  },
+  {
+    "revision": "345aff83c80e5cc52f62",
+    "url": "/static/js/runtime-main.9a173f38.js"
+  }
+]);
